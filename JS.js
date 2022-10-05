c43c808da3538f5d5cb603303dddc4e6d9d462e0
MrSergey98 <112868520+MrSergey98@users.noreply.github.com>
2022-10-05 18:53:47 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,1 +1,5 @@
-
+function calc(){
+var f1= parseInt(document.getElementById("kol").value);
+var f2= parseInt(document.getElementById("price").value);
+document.getElementById("result").innerHTML=f1*f2;
+}
