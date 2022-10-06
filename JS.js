d0a90e1a2abb52dd63b30c338b22647adf1d9d63
MrSergey98 <112868520+MrSergey98@users.noreply.github.com>
2022-10-06 22:41:16 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,5 +1,17 @@
-function calc(){
-var f1= parseInt(document.getElementById("kol").value);
-var f2= parseInt(document.getElementById("price").value);
-document.getElementById("result").innerHTML=f1*f2;
+function onClick(){
+let r=document.getElementById("result");
+r.innerHTML='';
+let f1 = document.getElementById("kol").value;
+let f2 = document.getElementById("price").value;
+    if((/\D/.test(f1))|(/\D/.test(f2)))
+        {
+        alert("!!!Допускаются только цифры: 0-9!!!")
+        return ;
+        }
+r.innerHTML=f1*f2;
 }
+
+document.addEventListener('DOMContentLoaded',function(event){
+let b = document.getElementById("btn");
+b.addEventListener("click", onClick);
+});
