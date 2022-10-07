d1a7058c163c9fa7b425c17087609475e8df5ff1
MrSergey98 <112868520+MrSergey98@users.noreply.github.com>
2022-10-07 12:25:32 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,17 +1,27 @@
-function onClick(){
-let r=document.getElementById("result");
-r.innerHTML='';
-let f1 = document.getElementById("kol").value;
-let f2 = document.getElementById("price").value;
-    if((/\D/.test(f1))|(/\D/.test(f2)))
-        {
-        alert("!!!Допускаются только цифры: 0-9!!!")
-        return ;
-        }
-r.innerHTML=f1*f2;
+var document;
+var alert;
+var console;
+
+function onClick() {
+    var r;
+    var f1;
+    var f2;
+    r = document.getElementById("result");
+    r.innerHTML = "";
+    f1 = document.getElementById("kol").value;
+    f2 = document.getElementById("price").value;
+    if ((/\D/.test(f1)) || (/\D/.test(f2))) {
+        alert("!!!Допускаются только цифры: 0-9!!!");
+        return;
+    }
+    r.innerHTML = f1 * f2;
+
 }
 
-document.addEventListener('DOMContentLoaded',function(event){
-let b = document.getElementById("btn");
-b.addEventListener("click", onClick);
+document.addEventListener("DOMContentLoaded", function (event) {
+    var b = document.getElementById("btn");
+    alert(event.type + " на " + event.currentTarget);
+    console.log("DOM fully loaded and parsed");
+    b.addEventListener("click", onClick);
+
 });
